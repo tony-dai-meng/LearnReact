3fcf484435095f5981fec957f129b3450f589400
Tony Dai <S65861@gmail.com>
2024-02-05 16:29:51 +1100

    State Machine and event handling

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
+
+
 function ListGroup() {
+  const items = ["Tokyo", "New York", "London", "Paris"];
+
+  // Hook to tap into built in features in React
+  const [selectedIndex, setSelectedIndex] = useState(-1)
+ // arr[0] //variable (selectedIndex)
+ // arr[1] //updater function
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item">Cras justo odio</li>
-      <li className="list-group-item">Dapibus ac facilisis in</li>
-      <li className="list-group-item">Morbi leo risus</li>
-      <li className="list-group-item">Porta ac consectetur ac</li>
-      <li className="list-group-item">Vestibulum at eros</li>
-    </ul>
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={selectedIndex === index ? "list-group-item active": "list-group-item"}
+            key={item + index}
+            onClick={() => {setSelectedIndex(index)}}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 export default ListGroup;
